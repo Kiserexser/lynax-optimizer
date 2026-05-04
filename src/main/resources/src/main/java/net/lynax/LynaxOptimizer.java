783a24b448f0b6154510b9dd570f0d6e70b795a6
Kiserexser <towartors@mail.ru>
2026-05-04 15:40:41 +0500

    Create LynaxOptimizer.java

---
+++ src/main/resources/src/main/java/net/lynax/LynaxOptimizer.java
@@ -0,0 +1,10 @@
+package net.lynax;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LynaxOptimizer implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Lynax Optimizer loaded!");
+    }
+}
